fb71a8db0f81a82ed39561ca7e1adb76d7902b19
NarasimhaAK <akn.narasimha@gmail.com>
2019-10-09 11:35:40 +0530

    updated the Java class

--- src/com/bt/general/HelloWorld.java
+++ src/com/bt/general/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to ANT !! " + getLocalCurrentDate());
+		System.out.println(" narasimha welcome to devops world !! " + getLocalCurrentDate());
 	}
 
 	private static Date getLocalCurrentDate() {
